f841d2539925528f307648960a70a932f3d487a5
prabesh <prabesh@localhost.localdomain>
2025-08-12 08:09:04 +0545

    lenis-js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import gsap from "gsap";
+import Lenis from 'lenis'
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
+  useEffect(() => {
+    // Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
+  })
   return (
     <main>
       <Navbar />
